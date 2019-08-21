f9dc96f494cae9fb55950373c98962a35315e98a
Richard Genthner <5654+moos3@users.noreply.github.com>
2019-08-21 12:12:41 -0400

    updated for makerbot

--- publish-alpha.sh
+++ publish-alpha.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 circleci config pack src > orb.yml
-circleci orb publish orb.yml iynere/compare-url@dev:alpha
+circleci orb publish orb.yml makerbot/compare-url@dev:alpha
 rm -rf orb.yml
